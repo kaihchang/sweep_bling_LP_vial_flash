0d5747be73d0544dff5f4f9b2a26cb30d7c7bcdb
kaihchang <kevinchang0527@gmail.com>
2023-10-17 10:15:14 +0800

    first commit

---
+++ keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF9, 0xE7, 0x73, 0x54, 0x49, 0xF0, 0x5A, 0xDF}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 3, 0, 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1, 0, 2, 4 }
